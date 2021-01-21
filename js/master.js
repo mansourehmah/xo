e84738416a7aaddb7b41e5acef7c2ddee4f60aeb
mansoureh <mansoureh.1998@gmail.com>
2021-01-21 21:31:54 +0330

    edit style

---
+++ js/master.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+  //timer function
+  setInterval(timeUpdate, 1000);
+  function timeUpdate() {
+    minute = $("#clock-minute").text();
+    second = $("#clock-second").text();
+
+    if (parseInt(second) === 59) {
+      var newMin = (parseInt(minute) + 1).toString();
+      if (newMin < 10) newMin = "0" + newMin;
+      $("#clock-minute").text(newMin);
+      $("#clock-second").text("00");
+    } else {
+      newSec = (parseInt(second) + 1).toString();
+      if (newSec < 10) newSec = "0" + newSec;
+      $("#clock-second").text(newSec);
+    }
+  }
+});
